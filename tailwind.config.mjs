d1f8b1e36820c1fac531a17289cfa914faedc46a
hectoraldairah <hectoraldairah@protonmail.com>
2024-12-14 00:13:12 -0600

    Rename to cat

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -29,7 +29,7 @@ export default {
         surface2: '#585b70',
         surface1: '#45475a',
         surface0: '#313244',
-        base: '#1e1e2e',
+        cat: '#1e1e2e',
         mantle: '#181825',
         crust: '#11111b',
       },
